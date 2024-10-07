d3d5bf92102cfbdbe1605e40b616973525824f4b
Aditi-Shukla17 <shuklaaditi129@gmail.com>
2024-10-07 19:21:51 +0530

    resolved conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import AppLanding from "./Components/AppLanding";
-import TopCarousel from "./Components/TopCarousel";
+import AppLanding from "../components/AppLanding";
+
 
 export default function Home() {
   return (
     <>
-    <TopCarousel/>
+
     <AppLanding/>
     </>
   );
